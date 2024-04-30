e13c4b131174864285c71a3bfd1da59f97a5d9b9
Miguel <juliofresno@modularbox.com>
2024-04-30 16:51:03 +0200

    Version AUDIOPLAYERS23

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:kplayer/kplayer.dart';
 import 'package:safey/app/routes/app_pages.dart';
 import 'package:safey/app/ui/pages/safey_page/safey_b.dart';
 
-void main() async {
-  Player.boot(); //add this  line (optional)
+void main()  {
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
     initialRoute: Routes.SAFEY,
