a01c1610fcb50081a67ce4df9d1f87cf587eac65
Miguel <juliofresno@modularbox.com>
2024-04-30 13:15:11 +0200

    Version AUDIOPLAYERS11

--- lib/main.dart
+++ lib/main.dart
@@ -5,22 +5,6 @@ import 'package:safey/app/ui/pages/safey_page/safey_b.dart';
 import 'package:simple_audio/simple_audio.dart';
 
 void main() async {
-  // Initialize with default values.
-  await SimpleAudio.init(
-    useMediaController: true,
-    shouldNormalizeVolume: false,
-    dbusName: "com.erikas.SimpleAudio",
-    actions: [
-      MediaControlAction.rewind,
-      MediaControlAction.skipPrev,
-      MediaControlAction.playPause,
-      MediaControlAction.skipNext,
-      MediaControlAction.fastForward,
-    ],
-    androidNotificationIconPath: "mipmap/ic_launcher",
-    androidCompactActions: [1, 2, 3],
-    applePreferSkipButtons: true,
-  );
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
     initialRoute: Routes.SAFEY,
