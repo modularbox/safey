c4bf07fc5a65ff22ef4e3f387fde3687bd209e2c
Miguel <juliofresno@modularbox.com>
2024-05-02 03:18:16 +0200

    Version version

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:get/get.dart';
 import 'package:safey/app/routes/app_pages.dart';
 import 'package:safey/app/ui/pages/safey_page/safey_b.dart';
 
-void main()  {
+void main() {
+  print("Version 1.0.0");
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
     initialRoute: Routes.SAFEY,
